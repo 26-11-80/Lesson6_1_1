4ac4b4da2a0551a1ca1f8b4fa1b5ba7d4d26ac01
anvarovagulnaz <anvarovagulnaz@gmail.com>
2021-12-04 14:28:25 +0500

    Добавьте файлы проекта.

---
+++ Lesson6_1_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите предложение");
+
+            string s = Console.ReadLine();
+
+            string longword = string.Empty;
+
+            string[] words = s.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+
+                if (words[i].Length > longword.Length)
+
+                    longword = words[i];
+
+            Console.WriteLine(longword);
+
+            Console.ReadKey();
+        }
+    }
+}
